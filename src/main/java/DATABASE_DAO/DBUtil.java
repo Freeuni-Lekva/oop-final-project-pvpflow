d05e9f482aae80ea7ebc7960c570e2b3bf90ddb0
Lodia15 <llodi22@freeuni.edu.ge>
2025-06-22 12:11:43 +0400

    Fix(build): Correctly remove duplicate variables in DBUtil and add message/friends

--- src/main/java/DATABASE_DAO/DBUtil.java
+++ src/main/java/DATABASE_DAO/DBUtil.java
@@ -6,13 +6,8 @@ import java.sql.SQLException;
 
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/quizapp";
-
     private static final String USER = "root";
     private static final String PASSWORD = "lukalodia";
-
-    private static final String USER = "quizapp";
-    private static final String PASSWORD = "";
-
 
     public static Connection getConnection() throws SQLException {
         try {
