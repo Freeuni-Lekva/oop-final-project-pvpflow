7b643a03a470ffcf30d6ece46c5aba773028106f
Lodia15 <llodi22@freeuni.edu.ge>
2025-07-12 02:10:47 +0400

    Update DBUtil changes

--- src/main/java/database/DBUtil.java
+++ src/main/java/database/DBUtil.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/quizapp";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "lukalodia";
 
     public static Connection getConnection() throws SQLException {
         System.out.println("DBUtil: Attempting to connect to database at " + URL);
